ce9172002499ea13ad447f395c4b3d36ec7b1cae
Emil Munkø <EMJ@kamstrup.com>
2017-10-12 09:29:30 +0200

    remvoed again. Chef server was the error

--- agents/files/RunJenkinsAgent.bat
+++
@@ -1,5 +0,0 @@
-echo off
-set username=%1
-set password=%2
-set name=%3
-java -jar C:/swarm-client.jar -master https://jenkins-omnisoft.devtools.kamstrup.dk -sslFingerprints " " -username %username% -password %password% -labels emil -name %name% -executors 10 -fsroot C:/Jenkins2 -mode exclusive -disableClientsUniqueId
