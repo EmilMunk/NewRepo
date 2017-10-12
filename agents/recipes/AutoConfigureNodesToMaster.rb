8c0eadd3dab796f5b751a5e12e013b22d080880f
Emil Munkø <EMJ@kamstrup.com>
2017-10-12 09:38:37 +0200

    The Job just hung after reboot. Trying something else

--- agents/recipes/AutoConfigureNodesToMaster.rb
+++ agents/recipes/AutoConfigureNodesToMaster.rb
@@ -5,42 +5,48 @@ log 'message' do
   level :info
 end
 
-cookbook_file 'C:\swarm-client.jar' do
-  source 'swarm-client.jar'
-  owner 'Administrator'
-  group 'Administrators'
-  mode '0755'
-end
-my_short_name = node.name
-log 'message' do
-  message my_short_name
-  level :info
-end
+# cookbook_file 'C:\swarm-client.jar' do
+#   source 'swarm-client.jar'
+#   owner 'Administrator'
+#   group 'Administrators'
+#   mode '0755'
+# end
+# my_short_name = node.name
+# log 'message' do
+#   message my_short_name
+#   level :info
+# end
 
-cookbook_file 'C:\runJenkinsAgent.bat' do
-  source 'RunJenkinsAgent.bat'
-  mode '0755'
-  owner 'Administrator'
-  group 'Administrators'
-  action :create
-end
+# cookbook_file 'C:\runJenkinsAgent.bat' do
+#   source 'RunJenkinsAgent.bat'
+#   mode '0755'
+#   owner 'Administrator'
+#   group 'Administrators'
+#   action :create
+# end
 
-file 'C:\runJenkinsAgentSetup.bat' do
-  content "RunJenkinsAgent emj Louise123402 #{my_short_name}"
-  mode '0755'
-  owner 'Administrator'
-  group 'Administrators'
-end
+# file 'C:\runJenkinsAgentSetup.bat' do
+#   content "RunJenkinsAgent emj Louise123402 #{my_short_name}"
+#   mode '0755'
+#   owner 'Administrator'
+#   group 'Administrators'
+# end
 
-cookbook_file 'C:\RegisterScheduledTask.ps1' do
-  source 'RegisterScheduledTask.ps1'
-  owner 'Administrator'
-  group 'Administrators'
-  mode '0755'
-end
+# cookbook_file 'C:\RegisterScheduledTask.ps1' do
+#   source 'RegisterScheduledTask.ps1'
+#   owner 'Administrator'
+#   group 'Administrators'
+#   mode '0755'
+# end
 
-powershell_script 'ExecuteTaskScheduler' do
-  code '. C:\RegisterScheduledTask.ps1'
-end
+# powershell_script 'ExecuteTaskScheduler' do
+#   code '. C:\RegisterScheduledTask.ps1'
+# end
 
+
+# Reboot after all is installed. 
+reboot 'app_requires_reboot' do
+  action :request_reboot
+  reason 'Need to cancel reboot when the run completes successfully.'
+end
 
